8f747d67573417c8a08e0432fc59a500a39aa780
GynShen <gynshen@gmail.com>
2026-05-19 00:33:20 +0800

    Blade template for view using if

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,4 +1,11 @@
-<x-header data="User"/>
-@if (!empty($user))
-	<h1>Hello, {{ $user }}</h1>
+<x-header data="User" />
+
+@if ($user == 'Peter')
+    <h2>Hello, {{ $user }}</h2>
+@elseif($user == 'Ali')
+    <h2>Apa khabar, {{ $user }}</h2>
+@elseif($user == 'Chong')
+    <h2>Ni Hao, {{ $user }}</h2>
+@else
+    <h2>Unknown User</h2>
 @endif
